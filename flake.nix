2ba2d6c5a5fba278791679c35b51197575bd0b8c
Peter Downs <github@peterdowns.com>
2023-02-25 19:04:33 -0500

    Bump package version.

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
           packages = rec {
             nix-search = pkgs.buildGoApplication {
               pname = "nix-search-cli";
-              version = "0.0.1";
+              version = "0.0.2";
               src = ./.;
               modules = ./gomod2nix.toml;
             };
